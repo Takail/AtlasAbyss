8701ac2cc958fdaa0fbb2e5917b8efe19ca3f44f
Takail <hancockjake1@gmail.com>
2024-01-12 01:08:34 +0000

    Initial Commit
    Initial Commit

---
+++ Patches/Route Patches.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+using HarmonyLib;
+
+using Object = UnityEngine.Object;
+
+namespace AtlasAbyss.Patches {
+    [HarmonyPatch(typeof(Terminal))]
+    public class RoutePatches {
+
+        [HarmonyPatch("LoadNewNode")]
+        [HarmonyPrefix]
+        private static void LoadNewNodePatchBefore(ref TerminalNode node) {
+            var terminal = Object.FindObjectOfType<Terminal>();
+
+            foreach (var noun in terminal.terminalNodes.allKeywords.First(terminalKeyword => terminalKeyword.word == "route").compatibleNouns) {
+                if (noun.result == null) {
+                    continue;
+                }
+
+                if (noun.result.name is "atlasabyssRoute") {
+                    noun.result.itemCost = Config.Instance.moonPrice;
+                }
+            }
+        }
+
+        [HarmonyPatch("LoadNewNodeIfAffordable")]
+        [HarmonyPrefix]
+        static void LoadNewNodeIfAffordablePatch(ref TerminalNode node) {
+            if (node == null || node.name != "atlasabyssRouteConfirm") {
+                return;
+            }
+            node.itemCost = Math.Abs(Config.Instance.moonPrice);
+        }
+    }
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace AtlasAbyss
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "AtlasAbyss";
+        public const string PLUGIN_NAME = "AtlasAbyss";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
